a950751423f140716bbf5eca0643589848ca27b4
Ondřej Všelko <ondrejvselko@gmail.com>
2025-03-31 12:02:33 +0200

    Navbar done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 function MyNavbar() {
   return (
       <div id="nav">
-        <a href="/"></a>
-        <a href="/edit"></a>
-        <a href="/stats"></a>
+          <a href="/edit"><img src="src/icons/history.png" alt ="history icon" id="nav_left"/></a>
+          <a href="/"><img src="src/icons/home.png" alt ="home icon" id="nav_mid"/></a>
+          <a href="/stats"><img src="src/icons/edit.png" alt ="edit icon" id="nav_right"/></a>
       </div>
   );
 }
